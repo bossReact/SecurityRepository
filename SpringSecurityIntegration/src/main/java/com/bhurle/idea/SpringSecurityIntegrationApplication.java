c8286793105abd24770cb5fb5b0004a7d871ab1e
CEPL <CEPL@DESKTOP-NSQGC9V>
2018-11-26 15:22:42 +0530

    all are done by admin

---
+++ SpringSecurityIntegration/src/main/java/com/bhurle/idea/SpringSecurityIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.bhurle.idea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIntegrationApplication.class, args);
+	}
+}
